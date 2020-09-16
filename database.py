164eb323a21f16bf51caf34ae2ac7bdab973a0fa
iamawesome99 <47467642+iamawesome99@users.noreply.github.com>
2020-09-16 18:30:08 -0400

    preserves roles

--- database.py
+++ database.py
@@ -1,21 +1,47 @@
 import json
 import psycopg2
 
-def connect():
-    credentials = json.load(open("secrets.json"))["database"]
+credentials = json.load(open("secrets.json"))["database"]
 
-    print('Connecting to the PostgreSQL database...')
-    conn = psycopg2.connect(**credentials)
+print('Connecting to the PostgreSQL database...')
+conn = psycopg2.connect(**credentials)
 
-    # create a cursor
-    cur = conn.cursor()
 
-    # execute a statement
-    print('PostgreSQL database version:')
-    cur.execute('SELECT version()')
+def get_cursor():
+    return conn.cursor()
 
-    # display the PostgreSQL database server version
-    db_version = cur.fetchone()
-    print(db_version)
 
-    return conn
+def commit():
+    return conn.commit()
+
+
+def create_role_table(curr):
+    curr.execute(
+        """
+        CREATE TABLE IF NOT EXISTS roles (
+            id serial PRIMARY KEY,
+            userid NUMERIC NOT NULL,
+            roleid NUMERIC NOT NULL
+        )
+        """
+    )
+
+    curr.execute(
+        "DELETE FROM roles"
+    )
+
+
+def create_role_row(curr, userid, roleid):
+    print("Adding", userid, roleid)
+    curr.execute("INSERT INTO roles (userid, roleid) VALUES ({}, {})".format(userid, roleid))
+
+
+def get_roles(curr, userid):
+    print("Getting roles for", userid)
+    curr.execute("SELECT roleid FROM roles WHERE userid={}".format(userid))
+    return curr.fetchall()
+
+
+def remove_role_row(curr, userid, roleid):
+    print("Deleting", userid, roleid)
+    curr.execute("DELETE FROM roles WHERE userid={} AND roleid={}".format(userid, roleid))
--- discordbot.py
+++ discordbot.py
@@ -8,32 +8,60 @@ class GClient(discord.Client):
 
     async def on_ready(self):
 
+        self.frozen_members = []
+
         secret = json.load(open("secrets.json"))
 
-        db = database.connect()
+        curr = database.get_cursor()
 
-        curr = db.cursor()
+        database.create_role_table(curr)
 
-        curr.execute(
-            """
-            CREATE TABLE roles (
-                id serial PRIMARY KEY,
-                userid NUMERIC NOT NULL,
-                roleid NUMERIC NOT NULL
-            )
-            """
-        )
+        self.server = self.get_guild(secret["serverID"])
 
-        server = self.get_guild(secret["serverID"])
-
-        for mem in server.members:
+        for mem in self.server.members:
             for role in mem.roles:
-                curr.execute("INSERT INTO roles (userid, roleid) VALUES ({}, {})".format(mem.id, role.id))
+                database.create_role_row(curr, mem.id, role.id)
 
-        db.commit()
+        database.commit()
+        curr.close()
+
+    async def on_member_join(self, member):
+
+        curr = database.get_cursor()
+
+        roles = database.get_roles(curr, member.id)
+
+        self.frozen_members.append(member.id)
+
+        print(roles)
+
+        for i in roles:
+            try:
+                await member.add_roles(self.server.get_role(int(i[0])))
+            except Exception as e:
+                pass
+
+        database.commit()
+        self.frozen_members.remove(member.id)
+        curr.close()
+
+    async def on_member_update(self, before, after):
+        if before.id in self.frozen_members:
+            print("skipping", before.id)
+            return
+
+        new_roles = list(set(after.roles) - set(before.roles))
+        old_roles = list(set(before.roles) - set(after.roles))
+
+        print(before.name, [i.name for i in new_roles], [i.name for i in old_roles])
+
+        curr = database.get_cursor()
+        for i in new_roles:
+            database.create_role_row(curr, before.id, i.id)
+
+        curr = database.get_cursor()
+        for i in old_roles:
+            database.remove_role_row(curr, before.id, i.id)
 
         curr.close()
-        db.close()
-
-        quit(21)
-
+        database.commit()

6f31616eb9c26cee669e2da11eb944a16912be29
iamawesome99 <47467642+iamawesome99@users.noreply.github.com>
2021-02-10 22:09:38 -0500

    added intents

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import discordbot
 import json
+import discord
 
 if __name__ == '__main__':
+
+    intents = discord.Intents(members=True, guilds=True, messages=True)
+
     secrets = json.load(open("secrets.json"))
-    client = discordbot.GClient()
+    client = discordbot.GClient(intents=intents)
     client.run(secrets["discordtoken"])
